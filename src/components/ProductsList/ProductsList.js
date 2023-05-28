62431004a849ff8366a76bed3e34126f6dd06261
AleksandrEnin163 <alexenin163@gmail.com>
2023-05-28 17:46:38 +0200

    second commit

---
+++ src/components/ProductsList/ProductsList.js
@@ -0,0 +1,13 @@
+import ProductItem from '../ProductItem/ProductItem';
+import s from './ProductsList.module.css'
+
+export default function ProductsList({ products, title }) {
+  return (
+    <div className={s.products_list_container}>
+      <h2>{title}</h2>
+      <div className={s.products_list_grid}>
+        {products.map(elem => <ProductItem product={elem} key={elem.id} />)}
+      </div>
+    </div>
+  )
+}
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -3,20 +3,25 @@ import DiscountForm from "../../components/DiscountForm/DiscountForm"
 import Head from "../../components/Head/Head"
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchCategoriesList } from '../../asyncActions/requests';
+import { fetchCategoriesList, fetchProductsList } from '../../asyncActions/requests';
+import ProductsList from "../../components/ProductsList/ProductsList";
 
 function MainPage() {
   const dispatch = useDispatch()
   const categories = useSelector(store => store.categories)
+  const products = useSelector(store => store.products)
 
   useEffect(() => { dispatch(fetchCategoriesList()) }, [])
+  useEffect(() => { dispatch(fetchProductsList()) }, [])
 
   const categories_main_page = categories.slice(0, 4)
+  const products_sale_main_page = products.filter(elem => elem.discont_price !== null).slice(0, 3)
   return (
     <div>
         <Head />
         <CategoriesList categories={categories_main_page} title='Catalog' btn={true} />
         <DiscountForm />
+        <ProductsList products={products_sale_main_page} title='Sale' />
     </div>
   )
 }
